96167f9d8ae08293904ebbb959bcc4039694f8e4
Tom Mason <masonworldt@gmail.com>
2021-07-31 17:13:52 +0100

    changed method of iterating files

--- scripts/generate_cmake_src_list.py
+++ scripts/generate_cmake_src_list.py
@@ -4,20 +4,33 @@ import sys
 
 def find_substring_in_list(list, sub, start = 0):
     for i in range(start, len(list)):
-        if list[i].index(sub) != -1:
+        if list[i].find(sub) != -1:
             return i
     return -1
 
-def replace_block_in_list(list, idx, size, new_block):
-    diff = len(new_block) - size
+def replace_block_in_list(list, idx, size_before, new_block):
+    diff = len(new_block) - size_before
     
     if diff > 0:
         for i in range(diff):
-            list.insert(idx, [0])
+            list.insert(idx, 0)
 
-        list[idx:idx + size] = new_block
+        list[idx:idx + len(new_block)] = new_block
 
+def recurse(dir):
+    contents = os.listdir(dir)
+    
+    files = []
+    for entry in contents:
+        entry = os.path.join(dir, entry)
+        if os.path.isdir(os.path.join(dir, entry)):
+            files.extend(recurse(entry))
+        else:
+            if '.h' in entry or '.c' in entry:
+                files.append(entry)
 
+    return files
+            
 
 def main():
     path = '../CMakeLists.txt'
@@ -29,28 +42,25 @@ def main():
 
         size_before = end_idx - add_call_idx
 
-        fun_call = contents[add_call_idx:end_idx]
+        fun_call = ['add_library(aubio\n']
         
-        fun_call[-1] = fun_call[-1].replace(')', '')
+        files = recurse('..\src')
+        def sort_key(file):
+            return (file.count('\\'), file)
+        files.sort(key=sort_key)
 
-        dir = os.listdir('../src') 
+        spaces = (' ' * len('add_library('))
+        for i in range(len(files)):
+            files[i] = spaces + files[i].replace('..\\', '') + '\n'
 
-        src = [s for s in dir if '.h' in s or '.c' in s]
+        files += ')\n'
 
-        src, subdirs = [], []
-        for entry in dir:
-            (subdirs, src)['.h' in entry or '.c' in entry].append(entry)
+        fun_call.extend(files)
 
-        fun_call.extend(src)
-
-        for sd in subdirs:
-            dir = os.listdir('../src/' + sd)
-            fun_call.extend(dir)
-
-        fun_call.sort()    
-        fun_call[-1] = fun_call[-1] + ')'
-
-
+        replace_block_in_list(contents, add_call_idx, size_before, fun_call)
+        
+        f.seek(0)
+        f.writelines(contents)
 
 if __name__ == '__main__':
     main()
